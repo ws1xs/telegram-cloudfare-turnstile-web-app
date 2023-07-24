b741e0b8cffa229d4e8ac5e63b92740a139339c6
mike <stainlessax+gith@protonmail.com>
2023-07-24 14:30:44 +0700

    1. Add types for window object. 2. Add success and error callbacks for Turnstile. 3. Set theme and size for Turnstile. 4. Remove a close button.

--- src/App.tsx
+++ src/App.tsx
@@ -5,40 +5,46 @@ import "./App.css";
 function App() {
   const { VITE_CLOUDFARE_SITE_KEY } = import.meta.env || {};
 
-  const onClose = (e: React.MouseEvent) => {
-    e.preventDefault();
+  const onCaptchaSuccess = () => {
+    console.log("Turnstile: Success.");
+
+    closeApp();
+  };
+
+  const onCaptchaError = () => {
+    console.log("Turnstile: Error.");
+
+    closeApp();
+  };
+
+  const closeApp = () => {
+    if (!WebApp) return;
 
     WebApp.close();
   };
 
   useEffect(() => {
-    console.log("cloudfare site key: ", VITE_CLOUDFARE_SITE_KEY);
+    if (!window) return;
 
-    // @ts-ignore
     window.onloadTurnstileCallback = () => {
       // @ts-ignore
       turnstile.render("#turnstile", {
         sitekey: VITE_CLOUDFARE_SITE_KEY,
         theme: "dark",
-        callback: (token: string) => {
-          console.log(`Challenge Success ${token}`);
-        },
+        size: "normal",
+        callback: onCaptchaSuccess,
+        "error-callback": onCaptchaError,
       });
     };
   }, [window]);
 
   useEffect(() => {
-    WebApp.ready();
+    WebApp?.ready();
   }, []);
 
   return (
     <div className="App">
       <div id="turnstile"></div>
-      <div>
-        <button className="Button" onClick={onClose}>
-          Close app
-        </button>
-      </div>
     </div>
   );
 }
