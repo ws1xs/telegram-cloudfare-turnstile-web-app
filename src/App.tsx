3357acaa18ec66f475629ce5e343fabb1c7e5162
mike <stainlessax+gith@protonmail.com>
2023-07-24 13:50:03 +0700

    Add turnstile into a separate div.

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ function App() {
   const { VITE_CLOUDFARE_SITE_KEY } = import.meta.env || {};
 
   useEffect(() => {
+    console.log("cloudfare site key: ", VITE_CLOUDFARE_SITE_KEY);
+
     // @ts-ignore
     window.onloadTurnstileCallback = () => {
       // @ts-ignore
-      turnstile.render("#app", {
+      turnstile.render("#turnstile", {
         sitekey: VITE_CLOUDFARE_SITE_KEY,
         callback: (token: string) => {
           console.log(`Challenge Success ${token}`);
@@ -17,7 +19,11 @@ function App() {
     };
   }, [window]);
 
-  return <div id="app" className="App"></div>;
+  return (
+    <div className="App">
+      <div id="turnstile"></div>
+    </div>
+  );
 }
 
 export default App;
